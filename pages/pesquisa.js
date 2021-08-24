8c45909a12ddfaa9a15d26c224601ec9200232d5
Epifron <braham.nt@gmail.com>
2021-08-23 22:12:37 -0300

    set the state of the form

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 export default function Pesquisa() {
+  const [form, setForm] = useState({
+    Nome: "",
+    Email: "",
+    Whatsapp: "",
+  });
   const save = async () => {
-    const form = {
-      Nome: "aa",
-      Email: "bb",
-      Whatsapp: "cc",
-    };
     try {
       const response = await fetch("/api/save", {
         method: "POST",
@@ -13,6 +15,14 @@ export default function Pesquisa() {
       const data = await response.json();
       console.log(data);
     } catch (err) {}
+  };
+  const onChange = (evt) => {
+    const value = evt.target.value;
+    const key = evt.target.name;
+    setForm((old) => ({
+      ...old,
+      [key]: value,
+    }));
   };
   return (
     <div className="pt-6">
@@ -25,13 +35,39 @@ export default function Pesquisa() {
       </p>
       <div className="w-60 mx-auto mt-2">
         <label className="font-bold">Seu nome:</label>
-        <input type="text" className="p-4 block shadow bg-input rounded my-1" />
+        <input
+          type="text"
+          className="p-4 block shadow bg-input rounded my-1"
+          placeholder="Nome"
+          onChange={onChange}
+          name="Nome"
+          value={form.nome}
+        />
+        <label className="font-bold">Seu Whatsapp:</label>
+        <input
+          type="text"
+          className="p-4 block shadow bg-input rounded my-1"
+          placeholder="Whatsapp"
+          onChange={onChange}
+          name="Whatsapp"
+          value={form.Whatsapp}
+        />
+        <label className="font-bold">Seu Email:</label>
+        <input
+          type="text"
+          className="p-4 block shadow bg-input rounded my-1"
+          placeholder="Email"
+          onChange={onChange}
+          name="Email"
+          value={form.Email}
+        />
         <button
           className="bg-button px-12 py-5 font-bold rounded-lg shadow-lg hover:shadow"
           onClick={save}
         >
           Enviar
         </button>
+        <pre>{JSON.stringify(form, null, 2)}</pre>
       </div>
     </div>
   );
